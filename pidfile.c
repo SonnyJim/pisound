301bb24061cee815624fea08fd04ae44e7084f83
SonnyJim <sonny_jim_pin@yahoo.com>
2014-10-13 22:26:43 +0100

    Add in PID file sanity

--- pidfile.c
+++ pidfile.c
@@ -6,7 +6,6 @@ int remove_pid (void)
 {
     if (verbose)
         fprintf (stdout, "Removing PID file %s\n", PIDFILE);
-
     if (remove (PIDFILE) != 0)
     {
         fprintf (stderr, "Error removing PID file %s: %s\n", PIDFILE, strerror(errno));
@@ -19,17 +18,38 @@ int remove_pid (void)
 int check_pid (void)
 {
     FILE *fd;
-    int pid;
+    int pid = getpid ();
+    int oldpid;
     
     //Check to see if pidfile already exists
-    if (access ( PIDFILE, R_OK ) == 0)
-    {   
-        fprintf (stderr, "Error: PID file %s already exists\n", PIDFILE);
-        fprintf (stderr, "Is pisound already running?\n");
-        return 1;
+    fd = fopen (PIDFILE, "r");
+    if (fd != NULL)
+    {  
+        //TODO Why does this give me such a fucking weird number?
+        //fread (&oldpid, sizeof(oldpid), 1, fd);
+        
+        //Read pid into oldpid
+        fscanf (fd, "%d", &oldpid);
+        
+        if (verbose)
+            fprintf (stdout, "PID file %s already exists, checking for process %d\n", PIDFILE, oldpid);
+        
+        //Check to see if the old process is still running
+        if (pid != oldpid && kill (oldpid, 0) == 0)
+        {
+            fprintf (stderr, "pisound is already running as PID %d\n", oldpid);
+            fclose(fd);
+            return 1;
+        }
+        else
+        {
+            //pidfile exists but process is no longer active, so we can remove the pid file
+            fclose(fd);
+            remove (PIDFILE);
+        }
     }
-  
-    //If not, make a pidfile
+
+    //Lets make a new pidfile!
     fd = fopen (PIDFILE, "w");
     if (fd == NULL)
     {
@@ -37,7 +57,7 @@ int check_pid (void)
         return 1;
     }
    
-    pid = getpid ();
+    //Write PID to file
     fprintf (fd, "%d\n", pid);
     fclose (fd);
     return 0;
