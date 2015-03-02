eedecb2e6afdc6085137df82d46ec2cf0f71e4cf
SonnyJim <sonny_jim_pin@yahoo.com>
2015-03-02 01:20:11 +0000

    Fix centring on generic scenes

--- src/scenes/hsentry.c
+++ src/scenes/hsentry.c
@@ -1,21 +1,31 @@
 #include "../pisound.h"
 #include "../gfx.h"
+#include "../scene.h"
+
+SDL_Texture * texture;
+SDL_Rect dstrect;
+
+static void init_hsentry_scene (void)
+{
+    if (verbose)
+        fprintf (stdout, "Initialising %s scene\n", scene_names[current_scene]);
+
+    SDL_Color color = { 255, 255, 255 };
+    SDL_Surface * surface = TTF_RenderText_Solid ( fonts[0], scene_names[current_scene], color);
+    texture = SDL_CreateTextureFromSurface (renderer, surface);
+    SDL_FreeSurface (surface);
+    SDL_QueryTexture (texture, NULL, NULL, &dstrect.w, &dstrect.h);
+    dstrect.x = (SCREEN_WIDTH / 2) - (dstrect.w / 2);
+    dstrect.y = (SCREEN_HEIGHT / 2) - (dstrect.h / 2);
+ 
+    running_scene = current_scene;
+}
 
 void draw_hsentry (void)
 {
-    int txtW, txtH;
+    if (current_scene != running_scene)
+        init_hsentry_scene ();
 
-    SDL_Color color = { 255, 255, 255 };
-    SDL_Surface * surface = TTF_RenderText_Solid ( fonts[0], "HIGH SCORE ENTRY", color);
-    SDL_Texture * texture = SDL_CreateTextureFromSurface (renderer, surface);
-    SDL_FreeSurface (surface);
-
-    SDL_QueryTexture (texture, NULL, NULL, &txtW, &txtH);
-    SDL_Rect dstrect = { SCREEN_WIDTH / 2, SCREEN_HEIGHT /2, txtW, txtH};
-    
     SDL_RenderCopy (renderer, texture, NULL, &dstrect); 
-    
-    SDL_DestroyTexture (texture);
-
 }
 
